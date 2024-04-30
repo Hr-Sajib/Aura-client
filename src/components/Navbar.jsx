9944be022d0c754ab98daf6d9878637f2ead69cc
hrSajib <harun15-4293@diu.edu.bd>
2024-04-30 23:53:20 +0600

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
 
     return (
         <div>
-            <div className="navbar bg-base-100 lg:pt-10 pb-5 lg:px-16 lg:mb-0 mb-40 animate__animated animate__fadeInDown">
+            <div className="navbar bg-base-100  lg:pt-10 pb-5 lg:px-16 lg:mb-0 mb-40 animate__animated animate__fadeInDown">
                 <div className="navbar-start">
                     <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
